ab84a0f8feb02717ab18379dddcf4ef7570a080e
Vinayak Vispute <93467074+VinayakVispute@users.noreply.github.com>
2023-03-28 02:10:57 +0530

    Pratical 2 Updated

---
+++ pratical1.java
@@ -0,0 +1,10 @@
+/*
+ * Use Cramer's rule to solve the following 2 X 2 system of linear equations for
+ * x and y:
+ * ax + by = e x = (ed - bf)/(ad - bc)
+ * cx + dy = f y = (af - ec)/(ad -bc)
+ * Write a Java program that prompts the user to enter the constants a, b, c, d,
+ * e, and f and the display the solution (x =?? and y =??). If ad-bc is 0, then
+ * your program prints “The equation has no solution".
+ * Note: (1) 3.4x+50.2y=44.5 (2) 2.1x+.55y=5.9
+ */
